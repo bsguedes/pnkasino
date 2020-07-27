4d50e8627011359c222a80000f6befc62ef81b8d
Bruno Guedes <bsguedes@inf.ufrgs.br>
2020-07-27 00:40:29 -0300

    methods for league listing

--- admin.py
+++ admin.py
@@ -11,7 +11,34 @@ admin = Blueprint('admin', __name__)
 @login_required
 def index():
     if current_user.is_admin_user():
-        return render_template('admin.html')
+        return render_template('admin.html', leagues=League.query.all())
+    else:
+        flash('User is not an admin.', 'error')
+        return redirect(url_for('main.profile'))
+
+
+@admin.route('/league/create', methods=['POST'])
+@login_required
+def league_create_post():
+    league_name = request.form.get('league_name')
+    if current_user.is_admin_user():
+        new_league = League(name=league_name, state='new')
+        db.session.add(new_league)
+        db.session.commit()
+        flash('League %s created successfully!' % league_name, 'success')
+        return redirect(url_for('admin.index'))
+    else:
+        flash('User is not an admin.', 'error')
+        return redirect(url_for('main.profile'))
+
+
+@admin.route('/league/edit', methods=['POST'])
+@login_required
+def history():
+    league_id = request.form.get('id')
+    if current_user.is_admin_user():
+        league = League.query.filter_by(id=int(league_id)).all()[0]
+        return render_template('edit.html', league=league)
     else:
         flash('User is not an admin.', 'error')
         return redirect(url_for('main.profile'))
