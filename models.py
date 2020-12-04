96497c239453711513e91bdb5f16e98a3524fe5c
Bruno Guedes <bsguedes@inf.ufrgs.br>
2020-12-04 10:39:22 -0300

    misses and hits on rankings

--- models.py
+++ models.py
@@ -29,18 +29,32 @@ class League(db.Model):
 
     def ranking(self):
         if self.state == 'finished':
-            user_points = {}
+            users = {}
             for category in self.categories:
                 opt = category.winner_option()
                 for bet in category.bets:
                     user_name = bet.user.name
-                    if user_name not in user_points:
-                        user_points[user_name] = 0
+                    if user_name not in users:
+                        users[user_name] = {
+                            'coins': 0,
+                            'hits': 0,
+                            'misses': 0
+                        }
                     result = 0 if opt is None else -bet.value if bet.option_id != opt.id else int(bet.value * opt.odds)
-                    user_points[user_name] += result
-            return [{'position': i + 1, 'name': name, 'coins': user_points[name]}
-                    for i, name in zip(range(len(user_points)),
-                                       sorted([n for n, _ in user_points.items()], key=lambda e: -user_points[e]))]
+                    users[user_name]['coins'] += result
+                    if result > 0:
+                        users[user_name]['hits'] += 1
+                    elif result < 0:
+                        users[user_name]['misses'] += 1
+            return [
+                {
+                    'position': i + 1,
+                    'name': name,
+                    'coins': users[name]['coins'],
+                    'hits': users[name]['hits'],
+                    'misses': users[name]['misses']
+                } for i, name in zip(range(len(users)),
+                                     sorted([n for n, _ in users.items()], key=lambda e: -users[e]['coins']))]
         else:
             return None
 
