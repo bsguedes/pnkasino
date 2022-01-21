686023c78d59cf7bdd75fc405ff7da357f0679fd
Thiago Lottici <thiagolottici@gmail.com>
2022-01-21 20:50:06 -0300

    Add react front-end

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # init.py
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_socketio import SocketIO
@@ -9,7 +9,7 @@ import os
 
 
 def create_app():
-    _app = Flask(__name__)
+    _app = Flask(__name__, static_folder="front-end/build")
 
     _app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
     _app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -43,6 +43,18 @@ def create_app():
 
     from roulette import start_roulette
     start_roulette()
+
+    @_app.route("/teste")
+    def serve():
+        """serves React App"""
+        return send_from_directory(_app.static_folder, "index.html")
+
+    @_app.route("/<path:path>")
+    def static_proxy(path):
+        """static folder serve"""
+        file_name = path.split("/")[-1]
+        dir_name = os.path.join(_app.static_folder, "/".join(path.split("/")[:-1]))
+        return send_from_directory(dir_name, file_name)
 
     return _app
 
